6d09501b4a48e6469f360b5dbab0b3784d9ad582
Nathan Szymanski <nathan.szymanski@berkeley.edu>
2021-01-22 13:12:44 -0800

    Added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Auto-XRD',
+    version='0.0.1',
+    description='A package designed to automate the process of phase identification from XRD spectra using a probabilistic deep learning trained with physics-informed data augmentation.',
+    author='Nathan J. Szymanski',
+    author_email='nathan_szymanski@berkeley.edu',
+    python_requires='>=3.6.0',
+    url='https://github.com/njszym/Auto-XRD',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['numpy', 'pymatgen', 'dtw-python', 'scipy', 'opencv-python', 'opencv-rolling-ball', 'keras', 'tensorflow']
+)
