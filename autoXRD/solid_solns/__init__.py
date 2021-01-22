89043ed6acd409f1c7d4dad367f25495fbc444aa
Nathan Szymanski <nathan.szymanski@berkeley.edu>
2021-01-21 16:45:22 -0800

    Simplified NS

--- autoXRD/solid_solns/__init__.py
+++ autoXRD/solid_solns/__init__.py
@@ -1,3 +1,4 @@
+from itertools import combinations as comb
 import warnings
 from pymatgen.core import periodic_table as pt
 import numpy as np
@@ -11,69 +12,69 @@ matcher = sm.StructureMatcher(scale=True, attempt_supercell=True, primitive_cell
 
 def tabulate_soluble_pairs(reference_directory):
     matching_pairs = []
-    for cmpd_A in os.listdir(reference_directory):
+    all_filenames = os.listdir(reference_directory)
+    for (cmpd_A, cmpd_B) in comb(filenames, 2):
         struct_A = mg.Structure.from_file('%s/%s' % (reference_directory, cmpd_A))
         formula_A = struct_A.composition.reduced_formula
-        for cmpd_B in os.listdir(reference_directory):
-            struct_B = mg.Structure.from_file('%s/%s' % (reference_directory, cmpd_B))
-            formula_B = struct_B.composition.reduced_formula
-            if formula_A != formula_B:
-                if matcher.fit(struct_A, struct_B):
-                    print(formula_A, formula_B)
-                    solubility = True
-                    comp_A = Composition(formula_A)
-                    comp_B = Composition(formula_B)
-                    probable_oxis_A = comp_A.oxi_state_guesses()
-                    probable_oxis_B = comp_B.oxi_state_guesses()
-                    if len(probable_oxis_A) == 0: ## This occurs for metals, in which case we'll just use atomic radii
-                        oxi_dict_A = {}
-                        for elem in [str(f) for f in comp_A.elements]:
-                            oxi_dict_A[elem] = 0.0
-                    else: ## Take most probable list of oxidation states
-                        oxi_dict_A = probable_oxis_A[0]
-                    if len(probable_oxis_B) == 0:
-                        oxi_dict_B = {}
-                        for elem in [str(f) for f in comp_B.elements]:
-                            oxi_dict_B[elem] = 0.0
-                    else:
-                        oxi_dict_B = probable_oxis_B[0]
-                    try:
-                        struct_B = matcher.get_s2_like_s1(struct_A, struct_B)
-                    except ValueError: ## Sometimes order matters
-                        struct_A = matcher.get_s2_like_s1(struct_B, struct_A)
-                    index = 0
-                    for (site_A, site_B) in zip(struct_A, struct_B):
-                        elem_A = ''.join([char for char in str(site_A.species_string) if char.isalpha()])
-                        elem_B = ''.join([char for char in str(site_B.species_string) if char.isalpha()])
-                        if solubility == True:
-                            site_A_oxi = oxi_dict_A[elem_A]
-                            site_B_oxi = oxi_dict_B[elem_B]
-                            if site_A_oxi.is_integer():
-                                if site_A_oxi == 0:
-                                    possible_rA = [pt.Element(elem_A).atomic_radius]
-                                else:
-                                    possible_rA = [pt.Element(elem_A).ionic_radii[site_A_oxi]]
-                            else:
-                                possible_rA = []
-                                possible_rA.append(pt.Element(elem_A).ionic_radii[int(np.floor(site_A_oxi))])
-                                possible_rA.append(pt.Element(elem_A).ionic_radii[int(np.ceil(site_A_oxi))])
-                            if site_B_oxi.is_integer():
-                                if site_B_oxi == 0:
-                                    possible_rB = [pt.Element(elem_B).atomic_radius]
-                                else:
-                                    possible_rB = [pt.Element(elem_B).ionic_radii[site_B_oxi]]
-                            else:
-                                possible_rB = []
-                                possible_rB.append(pt.Element(elem_B).ionic_radii[int(np.floor(site_B_oxi))])
-                                possible_rB.append(pt.Element(elem_B).ionic_radii[int(np.ceil(site_B_oxi))])
-                            possible_diffs = []
-                            for rA in possible_rA:
-                                for rB in possible_rB:
-                                    possible_diffs.append(abs(float(rA) - float(rB))/max([float(rA), float(rB)]))
-                            if min(possible_diffs) > 0.15:
-                                solubility = False
+        struct_B = mg.Structure.from_file('%s/%s' % (reference_directory, cmpd_B))
+        formula_B = struct_B.composition.reduced_formula
+        if formula_A != formula_B:
+            if matcher.fit(struct_A, struct_B):
+                print(formula_A, formula_B)
+                solubility = True
+                comp_A = Composition(formula_A)
+                comp_B = Composition(formula_B)
+                probable_oxis_A = comp_A.oxi_state_guesses()
+                probable_oxis_B = comp_B.oxi_state_guesses()
+                if len(probable_oxis_A) == 0: ## This occurs for metals, in which case we'll just use atomic radii
+                    oxi_dict_A = {}
+                    for elem in [str(f) for f in comp_A.elements]:
+                        oxi_dict_A[elem] = 0.0
+                else: ## Take most probable list of oxidation states
+                    oxi_dict_A = probable_oxis_A[0]
+                if len(probable_oxis_B) == 0:
+                    oxi_dict_B = {}
+                    for elem in [str(f) for f in comp_B.elements]:
+                        oxi_dict_B[elem] = 0.0
+                else:
+                    oxi_dict_B = probable_oxis_B[0]
+                try:
+                    struct_B = matcher.get_s2_like_s1(struct_A, struct_B)
+                except ValueError: ## Sometimes order matters
+                    struct_A = matcher.get_s2_like_s1(struct_B, struct_A)
+                index = 0
+                for (site_A, site_B) in zip(struct_A, struct_B):
+                    elem_A = ''.join([char for char in str(site_A.species_string) if char.isalpha()])
+                    elem_B = ''.join([char for char in str(site_B.species_string) if char.isalpha()])
                     if solubility == True:
-                        matching_pairs.append([cmpd_A, cmpd_B])
+                        site_A_oxi = oxi_dict_A[elem_A]
+                        site_B_oxi = oxi_dict_B[elem_B]
+                        if site_A_oxi.is_integer():
+                            if site_A_oxi == 0:
+                                possible_rA = [pt.Element(elem_A).atomic_radius]
+                            else:
+                                possible_rA = [pt.Element(elem_A).ionic_radii[site_A_oxi]]
+                        else:
+                            possible_rA = []
+                            possible_rA.append(pt.Element(elem_A).ionic_radii[int(np.floor(site_A_oxi))])
+                            possible_rA.append(pt.Element(elem_A).ionic_radii[int(np.ceil(site_A_oxi))])
+                        if site_B_oxi.is_integer():
+                            if site_B_oxi == 0:
+                                possible_rB = [pt.Element(elem_B).atomic_radius]
+                            else:
+                                possible_rB = [pt.Element(elem_B).ionic_radii[site_B_oxi]]
+                        else:
+                            possible_rB = []
+                            possible_rB.append(pt.Element(elem_B).ionic_radii[int(np.floor(site_B_oxi))])
+                            possible_rB.append(pt.Element(elem_B).ionic_radii[int(np.ceil(site_B_oxi))])
+                        possible_diffs = []
+                        for rA in possible_rA:
+                            for rB in possible_rB:
+                                possible_diffs.append(abs(float(rA) - float(rB))/max([float(rA), float(rB)]))
+                        if min(possible_diffs) > 0.15:
+                            solubility = False
+                if solubility == True:
+                    matching_pairs.append([cmpd_A, cmpd_B])
     unique_pairs = []
     for pair in matching_pairs:
         if set(pair) not in unique_pairs:
