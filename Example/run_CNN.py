5fd8e3512694fbb665e5baeeb5988fb8f01e88c2
Nathan Szymanski <nathan.szymanski@berkeley.edu>
2021-04-22 19:25:09 -0700

    Set confidence cutoff

--- Example/run_CNN.py
+++ Example/run_CNN.py
@@ -5,27 +5,43 @@ import pymatgen as mg
 import matplotlib.pyplot as plt
 
 
-max_phases = 3 # default: a maximum 3 phases in each mixture
-cutoff_intensity = 10 # default: ID all peaks with I >= 10% maximum spectrum intensity
-for arg in sys.argv:
-    if '--max_phases' in arg:
-        max_phases = int(arg.split('=')[1])
-    if '--cutoff_intensity' in arg:
-        cutoff_intensity = int(arg.split('=')[1])
+if __name__ == '__main__':
 
-spectrum_names, predicted_phases, confidences = spectrum_analysis.main('Spectra', 'References', max_phases, cutoff_intensity)
+    max_phases = 3 # default: a maximum 3 phases in each mixture
+    cutoff_intensity = 10 # default: ID all peaks with I >= 10% maximum spectrum intensity
+    for arg in sys.argv:
+        if '--max_phases' in arg:
+            max_phases = int(arg.split('=')[1])
+        if '--cutoff_intensity' in arg:
+            cutoff_intensity = int(arg.split('=')[1])
 
-for (spectrum_fname, phase_set, confidence) in zip(spectrum_names, predicted_phases, confidences):
+    spectrum_names, predicted_phases, confidences = spectrum_analysis.main('Spectra', 'References', max_phases, cutoff_intensity)
 
-    print('Filename: %s' % spectrum_fname)
-    print('Predicted phases: %s' % phase_set)
-    print('Confidence: %s' % confidence)
+    for (spectrum_fname, phase_set, confidence) in zip(spectrum_names, predicted_phases, confidences):
 
-    if '--plot' in sys.argv:
+        if '--all' not in sys.argv: # By default: only include phases with a confidence > 50%
+            all_phases = phase_set.split(' + ')
+            all_probs = [float(val[:-1]) for val in confidence]
+            final_phases, final_confidence = [], []
+            for (ph, cf) in zip(all_phases, all_probs):
+                if cf >= 50.0:
+                    final_phases.append(ph)
+                    final_confidence.append(cf)
 
-        # Format predicted phases into a list of their CIF filenames
-        predicted_phases = phase_set.split(' + ')
-        predicted_phases = ['%s.cif' % phase for phase in predicted_phases]
+            print('Filename: %s' % spectrum_fname)
+            print('Predicted phases: %s' % final_phases)
+            print('Confidence: %s' % final_confidence)
 
-        # Plot measured spectrum with line profiles of predicted phases
-        visualizer.main('Spectra', spectrum_fname, predicted_phases)
+        else: # If --all is specified, print *all* suspected phases
+            print('Filename: %s' % spectrum_fname)
+            print('Predicted phases: %s' % phase_set)
+            print('Confidence: %s' % confidence)
+
+        if '--plot' in sys.argv:
+
+            # Format predicted phases into a list of their CIF filenames
+            predicted_phases = phase_set.split(' + ')
+            predicted_phases = ['%s.cif' % phase for phase in predicted_phases]
+
+            # Plot measured spectrum with line profiles of predicted phases
+            visualizer.main('Spectra', spectrum_fname, predicted_phases)
