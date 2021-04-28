df04354c7600acbc29f25fb6f875d9d800b34e16
Nathan Szymanski <nathan.szymanski@berkeley.edu>
2021-04-28 07:41:48 -0700

    Copy autoXRD to path

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
+from shutil import copytree
+import os
 
+
+# Install dependencies
 setup(
     name='autoXRD',
     version='0.0.1',
@@ -12,3 +16,8 @@ setup(
     packages=find_packages(),
     install_requires=['numpy', 'pymatgen', 'dtw-python', 'scipy', 'opencv-python', 'opencv-rolling-ball', 'keras', 'tensorflow']
 )
+
+# Copy autoXRD to python path
+pythonpath = os.__file__.split('/')[:-1]
+pythonpath = '/'.join(pythonpath)
+copytree('autoXRD', '%s/autoXRD' % pythonpath)
