e8a215be19262487d79f10b2935d944b3f61509d
Nathan Szymanski <nathan.szymanski@berkeley.edu>
2022-06-08 10:12:20 -0700

    Cleaned up get-entries

--- Novel-Space/get-entries.py
+++ Novel-Space/get-entries.py
@@ -10,29 +10,29 @@ CIF files of all compositions.
 Simply specify the elements you wish to include (inc_elems).
 """
 
+# Which composition space to work in
 inc_elems = {'Li', 'Sn', 'In', 'O'}
 
+# Folder containin all CIF files
 os.chdir('ICSD/')
 all_entries = sorted(os.listdir('.'))
 
 total = len(all_entries)
-print(total)
-qi = 0
-for cmpd in all_entries:
+for index, cmpd in enumerate(all_entries):
 
-    qi += 1
-    if qi%100 == 0:
-        print('\n\n')
-        print(qi)
-        print('\n\n')
+    # Keep track of progress
+    if index%100 == 0:
+        print('%s/%s' % (index, total))
 
+    # Continue on if structure cannot load
     try:
         struc = Structure.from_file(cmpd)
     except:
-        print('Oh oh')
+        print('Structure failed to load')
         continue
 
     elems = set([str(e) for e in struc.composition.element_composition.elements])
 
+    # Only include structure if elems are subset of desired composition space
     if elems.issubset(inc_elems):
         shutil.copyfile(cmpd, '../All_CIFs/%s' % cmpd)
